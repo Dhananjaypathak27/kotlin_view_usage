83866eb4eae77f97690fdb96ce08b81bb1ee028e
dhananjaypathak27 <dj0027.dp@gmail.com>
2021-10-28 12:53:31 +0530

    UI updated

--- app/src/main/java/in/xparticle/test_app/RecyclerviewActivity.kt
+++ app/src/main/java/in/xparticle/test_app/RecyclerviewActivity.kt
@@ -4,10 +4,20 @@ import `in`.xparticle.test_app.adapter.TodoAdapter
 import `in`.xparticle.test_app.model.Todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.AbsListView
+import android.widget.TextView
+import androidx.transition.Fade
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
+import android.view.ViewGroup
+import androidx.transition.Slide
+
 
 class RecyclerviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +30,54 @@ class RecyclerviewActivity : AppCompatActivity() {
             Todo("pineapple",false),
             Todo("kaddu",true),
             Todo("gobhi",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("android",true),
+            Todo("banana",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("android",true),
+            Todo("banana",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("android",true),
+            Todo("banana",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("pineapple",false),
+            Todo("kaddu",true),
+            Todo("gobhi",false),
         )
 
         val adapter = TodoAdapter(list)
+        val image = findViewById<TextView>(R.id.textView)
+
 
         val recylerview =findViewById<RecyclerView>(R.id.rvTodo)
 
@@ -37,5 +92,41 @@ class RecyclerviewActivity : AppCompatActivity() {
         }
 
 
+        recylerview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0) {
+                    // Scrolling up
+                    val transition: Transition = Fade()
+                    transition.duration = 100;
+
+                    val view = findViewById<View>(android.R.id.content) as ViewGroup
+
+                    transition.addTarget(R.id.textView);
+                    TransitionManager.beginDelayedTransition(view, transition);
+                    image.visibility = View.GONE
+                } else {
+                    // Scrolling down
+                    val transition: Transition = Fade()
+                    transition.duration = 100;
+
+                    val view = findViewById<View>(android.R.id.content) as ViewGroup
+                    transition.addTarget(R.id.textView);
+                    TransitionManager.beginDelayedTransition(view, transition);
+                    image.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+                    // Do something
+                } else if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    // Do something
+                } else {
+                    // Do something
+                }
+            }
+        })
     }
 }
